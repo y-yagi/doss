97c8d79feb907c4a510eac622821bde6640cc9c3
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2022-01-16 09:54:07 +0900

    Don't use `RunE`
    `RunE` shows usage but it's not needed.

--- cmd/attach.go
+++ cmd/attach.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
@@ -10,12 +11,16 @@ import (
 var attachCmd = &cobra.Command{
 	Use:   "attach",
 	Short: "Attach a container",
-	RunE: func(cmd *cobra.Command, args []string) error {
+	Run: func(cmd *cobra.Command, args []string) {
 		executer, err := attach.NewExecuter(cmd, args, os.Stdout)
 		if err != nil {
-			return err
+			fmt.Fprintf(os.Stderr, "%v\n", err)
+			return
 		}
-		return executer.Execute()
+
+		if err := executer.Execute(); err != nil {
+			fmt.Fprintf(os.Stderr, "%v\n", err)
+		}
 	},
 }
 
